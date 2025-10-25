4b9401036dbb5c5114790d3a1f4436768820ede0
Rafa Almeida <98895577+EonianDaemonia@users.noreply.github.com>
2025-10-25 00:09:25 +0000

    Ajuste de porta para deploy

---
+++ index.js
@@ -0,0 +1,18 @@
+//Código do arquivo index.js Aula 24/10
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+ res.json({ message: "API rodando dentro do Codespaces!" });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+ console.log(`Servidor rodando na porta ${port}`);
+});
+
+// Importa as rotas de usuários
+const userRoutes = require("./routes/users");
+app.use("/users", userRoutes);
